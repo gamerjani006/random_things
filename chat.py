4cff91bfe652aff8bae2c6fa435ca84a292d79c9
gamerjani006 <zolihadur@gmail.com>
2022-09-25 12:00:39 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,116 +1,205 @@
 #!/usr/bin/env python3
 
-import http.cookiejar,urllib.parse,urllib.request
+import http.cookiejar, urllib.parse, urllib.request
 from http.cookies import SimpleCookie
 from json import loads as json_loads
-_headers={'Referer':'https://rentry.co'}
+
+_headers = {'Referer': 'https://rentry.co'}
+
+
 class UrllibClient:
-	def __init__(self):self.cookie_jar=http.cookiejar.CookieJar();self.opener=urllib.request.build_opener(urllib.request.HTTPCookieProcessor(self.cookie_jar));urllib.request.install_opener(self.opener)
-	def get(self,url,headers={}):request=urllib.request.Request(url,headers=headers);return self._request(request)
-	def post(self,url,data=None,headers={}):postdata=urllib.parse.urlencode(data).encode();request=urllib.request.Request(url,postdata,headers);return self._request(request)
-	def _request(self,request):response=self.opener.open(request);response.status_code=response.getcode();response.data=response.read().decode('utf-8');return response
-def raw(url):client=UrllibClient();return json_loads(client.get('https://rentry.co/api/raw/{}'.format(url)).data)
-def new(url,edit_code,text):client,cookie=UrllibClient(),SimpleCookie();cookie.load(vars(client.get('https://rentry.co'))['headers']['Set-Cookie']);csrftoken=cookie['csrftoken'].value;payload={'csrfmiddlewaretoken':csrftoken,'url':url,'edit_code':edit_code,'text':text};return json_loads(client.post('https://rentry.co/api/new',payload,headers=_headers).data)
-def edit(url,edit_code,text):client,cookie=UrllibClient(),SimpleCookie();cookie.load(vars(client.get('https://rentry.co'))['headers']['Set-Cookie']);csrftoken=cookie['csrftoken'].value;payload={'csrfmiddlewaretoken':csrftoken,'edit_code':edit_code,'text':text};return json_loads(client.post('https://rentry.co/api/edit/{}'.format(url),payload,headers=_headers).data)
+	def __init__(self):
+		self.cookie_jar = http.cookiejar.CookieJar()
+		self.opener = urllib.request.build_opener(
+			urllib.request.HTTPCookieProcessor(self.cookie_jar)
+		)
+		urllib.request.install_opener(self.opener)
+
+	def get(self, url, headers={}):
+		request = urllib.request.Request(url, headers=headers)
+		return self._request(request)
+
+	def post(self, url, data=None, headers={}):
+		postdata = urllib.parse.urlencode(data).encode()
+		request = urllib.request.Request(url, postdata, headers)
+		return self._request(request)
+
+	def _request(self, request):
+		response = self.opener.open(request)
+		response.status_code = response.getcode()
+		response.data = response.read().decode('utf-8')
+		return response
+
+
+def raw(url):
+	client = UrllibClient()
+	return json_loads(client.get('https://rentry.co/api/raw/{}'.format(url)).data)
+
+
+def new(url, edit_code, text):
+	client, cookie = UrllibClient(), SimpleCookie()
+	cookie.load(vars(client.get('https://rentry.co'))['headers']['Set-Cookie'])
+	csrftoken = cookie['csrftoken'].value
+	payload = {
+		'csrfmiddlewaretoken': csrftoken,
+		'url': url,
+		'edit_code': edit_code,
+		'text': text,
+	}
+	return json_loads(
+		client.post('https://rentry.co/api/new', payload, headers=_headers).data
+	)
+
+
+def edit(url, edit_code, text):
+	client, cookie = UrllibClient(), SimpleCookie()
+	cookie.load(vars(client.get('https://rentry.co'))['headers']['Set-Cookie'])
+	csrftoken = cookie['csrftoken'].value
+	payload = {'csrfmiddlewaretoken': csrftoken, 'edit_code': edit_code, 'text': text}
+	return json_loads(
+		client.post(
+			'https://rentry.co/api/edit/{}'.format(url), payload, headers=_headers
+		).data
+	)
+
 
 # Actually starting code lmfao
-#from dataclasses import dataclass, asdict
-#from ecdsa import SigningKey
+# from dataclasses import dataclass, asdict
+import ecdsa
+from ecdsa import SigningKey, VerifyingKey
 import PySimpleGUI as sg
-#import library
+import hashlib
+# import library
 import json
+import time
 
 
-class User: #TODO add proof of work
+class User:  # TODO add proof of work
 	def __init__(self, name):
 		self.current_room = None
+		self.current_key = None
 		self.username = name
-		#self.sk = SigningKey.generate()
-		#self.vk = self.sk.verifying_key
+		self.sk = SigningKey.generate()
+		self.vk = self.sk.verifying_key
 
-	def get_content(self, room_link):
-		self.full = raw(room_link)
-		self.content = self.full.get('content')
-		self.response = self.full.get('status')
-		
-		try:
-			self.infoline, self.chatline = self.content.split('\r\n')
-			self.chatline = json.loads(self.chatline)
-		except ValueError:
-			pass
+	def update(self, room_link):
+		full = raw(room_link)
+		self.content = full.get('content')
+		self.response = full.get('status')
 
-	def join_room(self, room_link): #TODO add an entry code(edit code), also use edit code as key
-		self.get_content(room_link)
-		if self.response != '200':
-			print('Error', self.response)
-			return False
+	def get_info(self, room_link):
+		self.update(room_link)
 		
-		try:
-			self.infoline, self.chatline = self.content.split('\r\n')
-			self.chatline = json.loads(self.chatline)
-			self.current_room = room_link
-			return True
-		except ValueError:
-			sg.Popup('Invalid room code!')
+		split = self.content.split('\r\n')
 		
+		self.infoline = split[0]
+		self.infoline_parsed = self.infoline.split(';')
+		
+		self.chat = json.loads(split[1])
+
+		self.current_room = room_link
+
 	def post(self, message, edit_code):
-		self.get_content(self.current_room)
-		self.chatline.append(f'{self.username}: {message}')
-		new_content = f'{self.infoline}\r\n{json.dumps(self.chatline)}'
+		self.update(self.current_room)
+		
+		format_message = {"username": self.username, "pubkey": self.vk.to_string().hex(), "message": message, "signature": self.sk.sign(message.encode()).hex()}
+		self.chat.append(format_message)
+		
+		new_content = f"{self.infoline}\r\n{json.dumps(self.chat)}"
+		
 		x = edit(self.current_room, edit_code, new_content)
 		return x
+
+def kdf(key: bytes, digest_length: int) -> bytes:
+	final = b''
+	current_byte = b''
+	for i in range(digest_length):
+		current_byte = hashlib.sha3_512(current_byte + key + str(i).encode() + str(digest_length).encode()).hexdigest()[i%64].encode()
+		final += current_byte
+		
+	return final
+
+
+def crypt(data: bytes, key: bytes) -> bytes:
+	key = kdf(key, len(data))
+	finished = bytes([i ^ key[c] for c,i in enumerate(data)])
+	return finished
+
+
+def parse_msg(message: dict):
+	pubkey = VerifyingKey.from_string(bytes(bytearray.fromhex(message.get("pubkey"))))
+	signature = bytes(bytearray.fromhex(message.get("signature")))
+	msg = message.get("message")
+	try:
+		verified = pubkey.verify(signature, msg.encode())
+		return f'{message.get("username")}: {msg}'
+	except ecdsa.keys.BadSignatureError:
+		print(f"Message {message}\nIs not verifiable!")
+		return None
 
 
 
 sg.theme('DarkAmber')
 
 login_layout = [
-[sg.Text('Input your nickname'), sg.In(size=(30,5), key='-NICK-')],
-[sg.Text('Input room code'), sg.In(size=(30,5), key='-ROOM-')],
-[sg.Button('Join', key='-JOIN-')]
+	[sg.Text('Input your nickname'), sg.In(size=(30, 5), key='-NICK-')],
+	[sg.Text('Input room code'), sg.In(size=(30, 5), key='-ROOM-')],
+	[sg.Button('Join', key='-JOIN-')],
 ]
 
 chat_layout = [
-[sg.Multiline('', disabled=False, size=(50,20), key='-CHATBOX-')],
-[sg.Text('Passwrd:'), sg.In(key='-EC-', size=(42))],
-[sg.Text('Message:'), sg.In(key='-MINP-', size=(35)), sg.Button('Send', key='-MSEND-')],
+	[sg.Multiline('', disabled=True, size=(50, 20), key='-CHATBOX-')],
+	[
+		sg.Text('Message:'),
+		sg.In(key='-MINP-', size=(35), do_not_clear=False),
+		sg.Button('Send', key='-MSEND-'),
+	],
 ]
 
-login=True
-
+login = True
 window = sg.Window('Join room', login_layout)
 
 while True:
 	event, values = window.read()
-	
+
 	if event == sg.WIN_CLOSED:
 		break
-	
+
 	if not login:
-		user.get_content(user.current_room)
-		window['-CHATBOX-'].Update(disabled=False)
-		window['-CHATBOX-'].Update('\n'.join(user.chatline))
-		window['-CHATBOX-'].Update(disabled=True)
-	
+		user.update(user.current_room)
+		#current = '\n'.join([parse_msg(i) for i in user.chat if parse_msg(i) != None])
+		#window['-CHATBOX-'].Update(current)
+
 	if event == '-MSEND-':
 		if len(values['-MINP-']) != 0:
-			x = user.post(values['-MINP-'], values['-EC-'])
+			_message = values['-MINP-']
+			x = user.post(_message, user.current_key)
 			if x.get('status') == '200':
-				window['-EC-'].Update(disabled=True)
-				window['-CHATBOX-'].Update(disabled=False)
-				window['-CHATBOX-'].Update('\n'.join(user.chatline))
-				window['-CHATBOX-'].Update(disabled=True)
+				current = '\n'.join([parse_msg(i) for i in user.chat if parse_msg(i) != None])
+				window['-CHATBOX-'].Update(current)
 			else:
-				sg.Popup(x)
+				sg.popup_ok(x.get('content'))
 
 	if event == '-JOIN-':
-		if len(values['-ROOM-']) > 1  and values['-NICK-'] != '' and len(values['-NICK-']) < 10 and len(values['-NICK-']) > 3:
+		if (
+			len(values['-ROOM-']) > 1
+			and len(values['-NICK-']) < 10
+			and len(values['-NICK-']) > 2
+		):
 			user = User(values['-NICK-'])
-			x = user.join_room(values['-ROOM-'])
-			if x and user.infoline=='-CHATROOM-':
-				login=False
+			user.get_info(values['-ROOM-'])
+			if user.infoline.startswith('chatroom'):
+				guess = None
+				key_hash = user.infoline_parsed[2]
+				salt = user.infoline_parsed[1]
+				while guess != key_hash:
+					_inp = sg.popup_get_text('Enter Edit Code')
+					guess = hashlib.blake2s(_inp.encode(), salt=salt.encode()).hexdigest()
+				login = False
+				user.current_key = _inp
 				window.close()
 				window = sg.Window('Chat', chat_layout, finalize=True)
-				window['-CHATBOX-'].Update('\n'.join(user.chatline))
-				window['-CHATBOX-'].Update(disabled=True)
-	#print(event, values)
+				current = '\n'.join([parse_msg(i) for i in user.chat if parse_msg(i) != None])
+				window['-CHATBOX-'].Update(current)
+		else:
+			sg.Popup('Something went wrong!')
